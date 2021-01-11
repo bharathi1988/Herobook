4a8d4b49c905d19245351803eb048c75e8c2ee1d
m_232294 <bharathi1988@gmail.com>
2021-01-11 17:40:50 -0600

    Initial Commit

---
+++ src/test/java/com/galvanize/andromeda/herobook/HerobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.andromeda.herobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
