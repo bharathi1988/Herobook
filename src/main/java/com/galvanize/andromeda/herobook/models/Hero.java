2b11067312ee25da26cf333cf15907d96894d8ab
Trotter <856439@cognizant.com>
2021-01-12 18:37:19 -0600

    Create integration test - adding items to persistence and view them

--- src/main/java/com/galvanize/andromeda/herobook/models/Hero.java
+++ src/main/java/com/galvanize/andromeda/herobook/models/Hero.java
@@ -1,10 +1,23 @@
 package com.galvanize.andromeda.herobook.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import java.util.UUID;
+
+@Entity
 public class Hero {
 
     private String heroName;
 
+    @Id
     private String id;
+
+    @PrePersist
+    private void ensureId(){
+        this.setId(UUID.randomUUID().toString());
+    }
 
     public String getHeroName() {
         return heroName;
---
+++ src/test/java/com/galvanize/andromeda/herobook/integrationTests/GetIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.galvanize.andromeda.herobook.integrationTests;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.galvanize.andromeda.herobook.models.Hero;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureDataJpa
+public class GetIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    @Test
+    public void getHeroes() throws Exception {
+        Hero superMan = new Hero();
+        superMan.setHeroName("Super Man");
+
+        mockMvc.perform(post("/herobooks/heroes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(superMan))
+               )
+            .andExpect(status().isCreated());
+
+        mockMvc.perform(get("/herobooks/heroes"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").exists())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$.[0].id").exists())
+                .andExpect(jsonPath("$.[0].heroName").value(superMan.getHeroName()))
+                ;
+    }
+
+}
