66347ba5511894caa98ed0cf5d6ff130873e92fc
m_232294 <bharathi1988@gmail.com>
2021-01-14 18:32:45 -0600

    Implemented the required properties for the Villain.java

--- src/test/java/com/galvanize/andromeda/herobook/unittests/controllers/VillainsControllerTests.java
+++ src/test/java/com/galvanize/andromeda/herobook/unittests/controllers/VillainsControllerTests.java
@@ -38,16 +38,35 @@ public class VillainsControllerTests {
 
     @BeforeEach
     public void setUp() {
-        joker = CreateVillain("Joker");
+        createJoker();
+
         catWoman = CreateVillain("Cat Woman");
         villains = new ArrayList<>();
         villains.add(joker);
         villains.add(catWoman);
     }
 
+    private void createJoker() {
+        joker = CreateVillain("Joker");
+        joker.setImage("URL");
+        joker.setArchRival("Batman");
+        joker.setRealName("Clark Kent");
+        joker.setHeight(75);
+        joker.setWeight(225);
+        joker.setSpecialPower("Powers of flight, superhuman strength, x-ray vision, heat vision, cold breath, super-speed, enhanced hearing, and nigh-invulnerability.");
+        joker.setIntelligence("Genius");
+        joker.setStrength("2 Billion Tons");
+        joker.setPower("Powers of flight, superhuman strength, x-ray vision, heat vision, cold breath, super-speed, enhanced hearing, and nigh-invulnerability.");
+        joker.setSpeed("2000 Miles/sec");
+        joker.setAgility("Superhuman Equilibrium");
+        joker.setStory("Superman was born on the planet Krypton and was given the name Kal-El at birth. As a baby, his parents sent him to Earth in a small spaceship moments before Krypton was destroyed in a natural cataclysm. His ship landed in the American countryside, near the fictional town of Smallville.");
+        joker.setDescription("Superman's powers include incredible strength, the ability to fly. X-ray vision, super speed, invulnerability to most attacks, super hearing, and super breath. He is nearly unstoppable. However, Superman does have one weakness, Kryptonite.");
+    }
+
     private Villain CreateVillain(String villainName) {
         Villain villain = new Villain();
         villain.setVillainName(villainName);
+
         return villain;
     }
 
@@ -73,7 +92,21 @@ public class VillainsControllerTests {
         mockMvc.perform(get("/herobooks/villains/Joker"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").exists())
-                .andExpect(jsonPath("$.villainName").value("Joker"));
+                .andExpect(jsonPath("$.villainName").value("Joker"))
+                .andExpect(jsonPath("$.image").value(joker.getImage()))
+                .andExpect(jsonPath("$.realName").value(joker.getRealName()))
+                .andExpect(jsonPath("$.height").value(joker.getHeight()))
+                .andExpect(jsonPath("$.weight").value(joker.getWeight()))
+                .andExpect(jsonPath("$.specialPower").value(joker.getSpecialPower()))
+                .andExpect(jsonPath("$.intelligence").value(joker.getIntelligence()))
+                .andExpect(jsonPath("$.strength").value(joker.getStrength()))
+                .andExpect(jsonPath("$.power").value(joker.getPower()))
+                .andExpect(jsonPath("$.speed").value(joker.getSpeed()))
+                .andExpect(jsonPath("$.agility").value(joker.getAgility()))
+                .andExpect(jsonPath("$.description").value(joker.getDescription()))
+                .andExpect(jsonPath("$.story").value(joker.getStory()))
+                .andExpect(jsonPath("$.archRival").value(joker.getArchRival()))
+        ;
 
         verify(mockVillainsService).findByVillainName("Joker");
     }
